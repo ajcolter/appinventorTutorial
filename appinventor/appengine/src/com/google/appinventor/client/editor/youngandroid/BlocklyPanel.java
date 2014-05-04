bde7ffea43d7192c984e77d526c6db643ea3053b
Joanie Weaver <jcweaver@jcweaver.local>
2014-05-03 21:00:45 -0400

    Tutorial is now set from choosing Walkthrough

--- appinventor/appengine/src/com/google/appinventor/client/editor/youngandroid/BlocklyPanel.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/youngandroid/BlocklyPanel.java
@@ -442,6 +442,9 @@ public class BlocklyPanel extends HTMLPanel {
     String currentScreen = Ode.getInstance().getDesignToolbar().getCurrentProject().currentScreen;
     doBackStep(proj_number+"_"+currentScreen);
   }
+  public static void callSetTutorial(){
+    doSetTutorial();
+  }
 
   /**
    * Remember any component instances for this form in case
@@ -907,6 +910,10 @@ public class BlocklyPanel extends HTMLPanel {
 
   public static native void doBackStep(String formName)/*-{
     $wnd.Tutorial.backStep(formName);
+  }-*/;
+
+  public static native void doSetTutorial()/*-{
+    $wnd.Tutorial.setTutorial("Tutorial_TalkToMe");
   }-*/;
 
   static native void setPreferredCompanion(String comp, String url) /*-{
